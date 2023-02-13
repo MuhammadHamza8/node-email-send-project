ce10bcb54b35c67c871a167ab38e8a75c1829441
Muhammad Hamza <64786334+MuhammadHamza8@users.noreply.github.com>
2023-02-13 18:11:14 +0500

    the project is sending email  from frontend in node js

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
+const express = require('express');
+const bodyParser  = require('body-parser');
+const app = express();
 const nodemailer =require('nodemailer');
+const path = require('path');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+const router = express.Router();
+
+ // Serve static files from the "public" directory
+app.use(express.static('public'));
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+router.post('/', function(req,res){
+
+  const{name,phone,email,subject,message} = req.body;
+   
+  const data ={
+    name:name,
+    phone:phone,
+    email:email,
+    subject:subject,
+    message:message
+  }
+  
+//   console.log(data);
+//   res.send(data);
+
+  // sending mail code............
+    
+
 
 const mailtransporter = nodemailer.createTransport({
 
@@ -15,9 +54,9 @@ const mailtransporter = nodemailer.createTransport({
 const details = {
 
     From:"hamzaicp54@gmail.com",
-    to: "carolineicp344@gmail.com",
-    subject:"The is test Email",
-    html:"<b>Thanks for contacting us We have recevied your contact details we will get back to you soon!!!</b>"
+    to: data.email,
+    subject:data.subject,
+    text:data.message
 };
 
 mailtransporter.sendMail(details,(err)=>{
@@ -28,3 +67,21 @@ mailtransporter.sendMail(details,(err)=>{
         console.log("email has sent ");
     }
 });
+    })
+// router.get('/about',function(req,res){
+//     res.sendFile(path.join(__dirname+'/about.html'));
+//   });
+ 
+
+
+
+ 
+
+ 
+//add the router
+
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+ 
+console.log('Running at Port 3000');
---
+++ mail.js
@@ -0,0 +1,30 @@
+const nodemailer =require('nodemailer');
+
+const mailtransporter = nodemailer.createTransport({
+
+    service:"gmail",
+    auth:{
+        user:"hamzaicp54@gmail.com",
+        pass:"uhapeyqhunsvjohj"
+
+    }
+});
+
+
+
+const details = {
+
+    From:"hamzaicp54@gmail.com",
+    to: "hamzaicp53@gmail.com",
+    subject:"The is test Email",
+    html:"<b>Thanks for contacting us We have recevied your contact details we will get back to you soon!!!</b>"
+};
+
+mailtransporter.sendMail(details,(err)=>{
+    if(err){
+        console.log("somethigs went wrong");
+    }else
+    {
+        console.log("email has sent ");
+    }
+});
